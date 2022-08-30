2196a5f8c2ebcc32a609ead7afa20604f26abd08
kade-cooper <112003731+kade-cooper@users.noreply.github.com>
2022-08-30 12:33:28 -0400

    worked on design

--- labs/lab1/js/app.js
+++ labs/lab1/js/app.js
@@ -1,6 +1,8 @@
 function setup(){
-    createCanvas(800,600);
-    background()
+    createCanvas(800,400);
+    background(0,222,155);
+    fill("orange");
+    triangle(100,150,0,100,100,50);
     for(var i=0; i <= 20;i++){
         if(i%2==0){
             fill("orange");
@@ -21,11 +23,31 @@ function setup(){
     }
     for(var i=20; i >= 0;i--){
         if(i%2==0){
+            fill("blue");
+        }
+        else{
+            fill("orange");
+        }
+        circle(100+i*30,190, 50);
+    }
+    for(var i=0; i <= 3;i++){
+        if(i%2==0){
+            fill("blue");
+        }
+        else{
+            fill("orange");
+        }
+        circle(100,190+i*30, 50);
+    }
+    for(var i=0; i <= 20;i++){
+        if(i%2==0){
             fill("orange");
         }
         else{
             fill("blue");
         }
-        circle(100+i*30,190, 50);
+        circle(100+i*30,280, 50);
     }
+    rect(650,230,100,100);
+    
 }

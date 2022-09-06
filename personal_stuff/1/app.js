f9e6cc694349eadc9f02c43df645406b055d7721
kade-cooper <112003731+kade-cooper@users.noreply.github.com>
2022-09-06 11:22:57 -0400

    made game thing

---
+++ personal_stuff/1/app.js
@@ -0,0 +1,28 @@
+function setup(){
+    createCanvas(800,800);
+    var timer;
+    textSize(24);
+    textAlign(CENTER);
+}
+var score=0;
+var randX;
+var randY;
+function draw(){
+    background(100);
+    line(mouseX-800,mouseY,mouseX+800,mouseY)
+    line(mouseX,mouseY-800,mouseX,mouseY+800)
+    timer=millis();
+    if(timer%2000<=20){
+    randX = random(0,700);
+    randY = random(0,700);
+    fill("white");
+    }
+    console.log(timer);
+    var d = dist(mouseX,mouseY,randX,randY);
+    if(d<80 && mouseIsPressed){
+        fill("red");
+        score+=1;
+    }
+    text(score.toString(),60,60)
+    circle(randX,randY,80);
+}

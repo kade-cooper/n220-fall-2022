e35a4c1fc9cd25e616752ea8c33f96e2069ef793
kade-cooper <112003731+kade-cooper@users.noreply.github.com>
2022-09-20 12:25:25 -0400

    finished lab 3

--- labs/lab4/bouncing_ball/js/app.js
+++ labs/lab4/bouncing_ball/js/app.js
@@ -1,3 +1,26 @@
 function setup(){
-    createCanvas(800,800);
+    createCanvas(800,600);
+}
+
+function ball(size,color,xvel,yvel){
+    this.size=size;
+    this.color=color;
+    this.xvel=xvel;
+    this.yvel=yvel;
+}
+ballx=100;
+bally=100;
+ball1=new ball(20,"red",2.5,2.5);
+function draw(){
+    background("white");
+    fill(ball1.color);
+    circle(ballx,bally,ball1.size);
+    ballx+=ball1.xvel;
+    bally+=ball1.yvel;
+    if(ballx>=800 || ballx<=0){
+        ball1.xvel*=-1;
+    }
+    if(bally>=600 || bally<=0){
+        ball1.yvel*=-1;
+    }
 }

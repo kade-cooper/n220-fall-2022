cd1d87b349ff16f19a12b8e1a2caabf4dc8e5e42
kade-cooper <112003731+kade-cooper@users.noreply.github.com>
2022-10-05 12:58:10 -0400

    added comments

--- labs/lab6/LoopButBig/js/app.js
+++ labs/lab6/LoopButBig/js/app.js
@@ -1,5 +1,6 @@
 //Kade Cooper 10/4/2022 N220
 let dvBox = document.getElementById("box");
+//loops 5 times incrementing 1000 each time
 for(let i=1000;i<=5000;i+=1000){
     dvBox.innerHTML += i + " ";
 }

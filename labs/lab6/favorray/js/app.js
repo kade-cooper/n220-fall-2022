6de59f571ead29a60f499f52fd03e9d5906c689f
kade-cooper <112003731+kade-cooper@users.noreply.github.com>
2022-11-02 12:01:25 -0400

    added comment

--- labs/lab6/favorray/js/app.js
+++ labs/lab6/favorray/js/app.js
@@ -1,6 +1,7 @@
 //Kade Cooper 10/4/2022 N220
 let dvBox = document.getElementById("box");
 let favorites = ['Gaming','Pizza','Tech','White rice','Steak']
+//loops the lengh of favorites times
 for(let i=0;i<favorites.length;i++){
     dvBox.innerHTML += favorites[i] + ", is one of my favorite things." + '<br>';
 }

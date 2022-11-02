1c8beb67059c8b1da023d79cdb350ad1ad931892
kade-cooper <112003731+kade-cooper@users.noreply.github.com>
2022-11-02 12:23:17 -0400

    did lab assignment

---
+++ labwork/labAssignment6/js/app.js
@@ -0,0 +1,30 @@
+//Kade Cooper 11/2/2022 N220
+let inp=document.getElementById("input");
+let out=document.getElementById("output");
+
+function checkVowel(character){
+    let str=character.toLowerCase()
+    if(str=="a" || str=="e" || str=="i" || str=="o" || str=="u"
+    || str=="y"){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+
+
+function removeVowels(strg){
+    let newStr = "";
+    for(var i=0; i<strg.length; i++){
+        if(checkVowel(strg[i])==false){
+            newStr+=strg[i];
+        }
+    }
+return newStr;
+}
+
+function clicked(){
+    out.innerHTML = removeVowels(inp.value);
+}

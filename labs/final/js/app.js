9097b7d6eb9046ffd8c6525bca1a0661f60cceaa
kade-cooper <112003731+kade-cooper@users.noreply.github.com>
2022-11-16 13:19:35 -0500

    worked on final

--- labs/final/js/app.js
+++ labs/final/js/app.js
@@ -2,8 +2,10 @@ let display = document.getElementById("display");
 
 
 
+
 function start(){
     let arr=[];
+    let roundMax=3;
     //first for loop makes array upon start button being pressed
     for(let i =0;i<8;i++){
         let rand=Math.floor(Math.random()*4);
@@ -20,27 +22,40 @@ function start(){
             arr[i]="ArrowUp";
         }
     }
-    //loops through array starting with first 3 then first 4 etc
-    for(let j=3;j<arr.length;j++){
-        for(let g=0;g<j;g++){
-            console.log(arr[g]);
-            display.innerHTML=arr[g];
-        }
-        //not done yet but will detect user input to check if they got the correct order
-        let response=[];
-        document.onkeydown=function(event){
-            response+=event.key;
-               
-            console.log(response);
-        }
-        //checks that user input array (response) is equal to the portion of the main array
-        for(let h=0;h<j;h++){
-            if(response[h]!=arr[h]){
-                break;
+    //loops through array roundMax times
+            let curIndex=0;
+            setTimeout(displayPrompt,3000)
+            function displayPrompt(){
+                display.innerHTML=arr[curIndex];
+                curIndex++;
+                if(curIndex<roundMax+1){
+                    setTimeout(displayPrompt,1000);
+                } 
+                else{
+                    display.innerHTML="waiting for input";
+                }
             }
-        }
-        console.log("break"+j);
-    }
+            let response=[];
+            //detects key presses
+            document.onkeydown=function(event){
+                console.log(response);
+                if(response.length<roundMax){
+                        response.push(event.key);
+                }
+                else{
+                    for(let h=0;h<roundMax;h++){
+                        if(response[h]!=arr[h]){
+                            return;
+                        }
+                    }
+                    response=[];
+                    roundMax++;
+                    curIndex=0;
+                    setTimeout(displayPrompt,3000)
+                }
+                console.log(response);
+            }
+            //checks that user input array (response) is equal to the portion of the main array
+            console.log("break");
     console.log(arr);
-}
-
+    }
